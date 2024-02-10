1ee5f149d99b0282cc5e45ff4072d6138fd7ed3f
David Inonda <davidinonds@gmail.com>
2024-02-10 23:02:20 +0300

    xvbbbc
    cnmmghvn h

--- ebenezaschool/settings.py
+++ ebenezaschool/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qy(ga%b9-s*6ooyyo+o*u(7%axw#x8usuue)#$azv2dm39f#)0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
